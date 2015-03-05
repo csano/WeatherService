141470d3a715d6e2491cda5234635f0b5dbc9989
Chris Sano <csano@microsoft.com>
2015-03-05 11:41:33 -0800

    Initial commit

---
+++ WeatherService.UI/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace WeatherService.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var apiProxy = new ApiProxy();
+            var weather = await apiProxy.MakeRequest();
+
+            temperature.Text = weather.Temperature.ToString(CultureInfo.InvariantCulture);
+            location.Text = weather.Location.City;
+        }
+    }
+}
---
+++ WeatherService/ApiProxy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace WeatherService
+{
+    public class Location
+    {
+        public string City { get; set; }
+    }
+
+    public class Weather
+    {
+        public Weather(Location location)
+        {
+            Location = location; 
+        }
+        public Location Location { get; private set; }
+        public double Temperature { get; set; } 
+    }
+
+    public class ApiProxy
+    {
+        public async Task<Weather> MakeRequest()
+        {
+            Weather weather = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/");
+
+                var response = await client.GetAsync("weather/?q=Seattle,WA&mode=xml");
+                if (response.IsSuccessStatusCode)
+                {
+                   var output = await response.Content.ReadAsStringAsync();
+                   var doc = XDocument.Parse(output);
+                   var city = doc.XPathSelectElement("/current/city").Attribute("name").Value;
+                   var temperature = Double.Parse(doc.XPathSelectElement("/current/temperature").Attribute("value").Value);
+                   var location = new Location { City = city };
+                   weather = new Weather(location) { Temperature = temperature };
+                }
+            }
+
+            return weather;
+        }
+    }
+}
