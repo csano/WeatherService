a00ce2228a67cbf876adb58acd70763750f40e4f
Chris Sano <csano@microsoft.com>
2015-03-05 22:16:39 -0800

    Inverted if statement

--- WeatherService/OpenweathermapWeatherService.cs
+++ WeatherService/OpenweathermapWeatherService.cs
@@ -30,21 +30,21 @@ namespace WeatherService
     {
         public async Task<Weather> GetWeatherAsync(string query)
         {
-            Weather weather = null;
+            Weather weather;
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/");
 
                 var response = await client.GetAsync(string.Format("weather/?q={0}&mode=xml", query));
-                if (response.IsSuccessStatusCode)
-                {
-                   var output = await response.Content.ReadAsStringAsync();
-                   var doc = XDocument.Parse(output);
-                   var city = doc.XPathSelectElement("/current/city").Attribute("name").Value;
-                   var temperature = Double.Parse(doc.XPathSelectElement("/current/temperature").Attribute("value").Value);
-                   var location = new Location { City = city };
-                   weather = new Weather(location) { Temperature = temperature };
-                }
+
+                if (!response.IsSuccessStatusCode) return null;
+
+                var output = await response.Content.ReadAsStringAsync();
+                var doc = XDocument.Parse(output);
+                var city = doc.XPathSelectElement("/current/city").Attribute("name").Value;
+                var temperature = Double.Parse(doc.XPathSelectElement("/current/temperature").Attribute("value").Value);
+                var location = new Location { City = city };
+                weather = new Weather(location) { Temperature = temperature };
             }
 
             return weather;
